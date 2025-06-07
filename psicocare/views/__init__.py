d0fa5e4be8e1493016c258c1020ef7755c5f326f
Cíntia Cunha <90116425+cintiacc@users.noreply.github.com>
2025-06-06 23:29:46 -0300

    update init.py

--- psicocare/views/__init__.py
+++ psicocare/views/__init__.py
@@ -3,4 +3,5 @@ from .patient_view import PatientViewSet
 from .psychologist_view import PsychologistViewSet
 from .therapy_plan_view import TherapyPlanViewSet
 from .session_view import SessionViewSet
+from .payment_view import PaymentViewSet
 
