3205e0da78a655e865a540e532889c7bde37e013
Cíntia Cunha <90116425+cintiacc@users.noreply.github.com>
2025-06-06 23:05:09 -0300

    update init.py

--- psicocare/urls/__init__.py
+++ psicocare/urls/__init__.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('users/', include('psicocare.urls.user_urls')),
     path('patients/', include('psicocare.urls.patient_urls')),
     path('psychologists/', include('psicocare.urls.psychologist_urls')),
+    path('therapy-plans/', include('psicocare.urls.therapy_plan_urls')),
 ]
