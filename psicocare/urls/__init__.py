ae3049e24f12fd7da1aa0b852cbb21abc459cd13
Cíntia Cunha <90116425+cintiacc@users.noreply.github.com>
2025-06-06 22:55:14 -0300

    update init.py

--- psicocare/urls/__init__.py
+++ psicocare/urls/__init__.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('users/', include('psicocare.urls.user_urls')),
     path('patients/', include('psicocare.urls.patient_urls')),
+    path('psychologists/', include('psicocare.urls.psychologist_urls')),
 ]
