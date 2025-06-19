5a5de8f6bf62470bf7facc94a68b1bd9fb2e9394
Cíntia Cunha <90116425+cintiacc@users.noreply.github.com>
2025-06-19 14:45:10 -0300

    update urls.py

--- psicocare/api/urls.py
+++ psicocare/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import get_user, create_user, get_patients, create_patient, get_psychologists, create_psychologist
+from .views import get_user, create_user, get_patients, create_patient, get_psychologists, create_psychologist, get_therapy_plans, create_therapy_plan
+
 
 urlpatterns = [
     path('users/', get_user, name='get_user'),
@@ -10,4 +11,7 @@ urlpatterns = [
 
     path('psychologists/', get_psychologists, name='get_psychologists'),
     path('psychologists/create/', create_psychologist, name='create_psychologist'),
+
+    path('therapy-plans/', get_therapy_plans, name='get_therapy_plans'),
+    path('therapy-plans/create/', create_therapy_plan, name='create_therapy_plan'),
 ]
