c8c68ffa5561ab1a1e3df4351d3fb5f6c1546f3d
Cíntia Cunha <90116425+cintiacc@users.noreply.github.com>
2025-06-19 13:23:52 -0300

    update patient serializer

--- psicocare/api/serializer.py
+++ psicocare/api/serializer.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Patient
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'name', 'email', 'senha', 'tipo']
+        read_only_fields = ['id']
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = ['id', 'idade', 'genero', 'telefone', 'plano_saude', 'user']
         read_only_fields = ['id']
